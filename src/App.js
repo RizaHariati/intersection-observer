b0ea5b71c593ed5c54482dd6cf79730bbab50e38
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2022-04-28 16:17:05 +0700

    random tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useCallback, useEffect } from "react";
 
-function App() {
+const useChangeView = (options) => {
+  const [ref, setRef] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+    if (ref) observer.observe(ref);
+
+    return () => {
+      if (ref) observer.unobserve(ref);
+    };
+  }, [ref, options]);
+
+  return { setRef, visible };
+};
+
+const App = () => {
+  // const ref = { current: null };
+  // const visible = false;
+
+  const { setRef, visible } = useChangeView({ threshold: 0.2 });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ height: "100vh" }}>
+        <h1>
+          Scroll down to the next section{" "}
+          <span role="img" aria-label="pointing down">
+            &#128071;
+          </span>
+        </h1>
+      </div>
+      <div
+        ref={setRef}
+        style={{
+          height: "100vh",
+          transition: "all 1s ease-in",
+          backgroundColor: visible ? "pink" : "lightGray",
+        }}
+      >
+        {visible ? (
+          <h1>
+            I'm Visible
+            <span role="img" aria-label="pointing up">
+              &#9757;
+            </span>
+          </h1>
+        ) : (
+          <h1>
+            you need to change this page
+            <span role="img" aria-label="pointing down">
+              &#128071;
+            </span>
+          </h1>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
